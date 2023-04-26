23064e287b2a0d700af8bd661420536201c64bd2
Anthony Ribeiro da Mata <anthonyrmataa@gmail.com>
2023-04-26 16:15:24 -0300

    Made some css adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import { UsernameInfo } from './components/UsernameInfo'
 export function App() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="relative max-h-[600px] max-w-[420px] flex flex-col">
-        <div className="w-full mb-12 overflow-auto bg-black scrollbar scrollbar-w-1 scrollbar-thumb-rounded-full scrollbar-thumb-neutral-700">
+      <div className="relative h-full max-w-[500px] flex flex-col bg-black">
+        <div className="w-full mb-12 overflow-auto scrollbar scrollbar-w-1 scrollbar-thumb-rounded-full scrollbar-thumb-neutral-700">
           <Header />
 
           <UserProfileData />
